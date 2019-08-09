04e4be1f11e9427b6be4bdca7cb5fc886f213747
Keqiang Li <lkq@umich.edu>
2019-08-09 17:05:50 -0400

    removed unnecessary flag

--- seqidx/seqidx.go
+++ seqidx/seqidx.go
@@ -19,9 +19,7 @@ func main() {
 
 	rVerPtr := flag.String("r", "30", `Annotation source release version; if you specified "gencode" as the annotation source, you need to specify version "30" or above for Human and version "M22" or above for Mouse. If you specified "ensembl", you just need to specify the ensembl release version "96" or above`)
 
-	filesExisted := flag.Bool("e", false, `Annotation files already exist. You must specify -d as the directory which contains all your annotations files`)
-
-	annotationFileDir := flag.String("d", "", `Directory that contains the genome annotation files`)
+	existingDir := flag.String("d", "", "Use this option to indicate the annotation files already exist and specify the directory which contains all your annotations files; omit this option if you want to download annotation files")
 
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
@@ -37,7 +35,7 @@ func main() {
 		fmt.Println()
 		fmt.Print(`Examples
   seqidx
-    will run using the default values, which is equivalent to seqidx -t rna -a gencode -s hs -r 30; this will download Gencode annotation files for Human Gencode version 30 and build RNA-seq index files
+    run using the default values, which is equivalent to seqidx -t rna -a gencode -s hs -r 30; this will download Gencode annotation files for Human Gencode version 30 and build RNA-seq index files
 
   seqidx -s mm -r M22
     downloads Gencode annotation files for Mouse Gencode version M22 and build RNA-seq index files
@@ -45,26 +43,24 @@ func main() {
   seqidx -a ensembl -r 96 -s dm
     downloads Ensembl annotation files for Fruitfly Ensembl version 96 and build RNA-seq index files
 
-  seqidx -t chip -a ensembl -r 96 -s dm -e -d ensembl_dm_96
-    Using Ensembl annotation files under directory 'ensembl_dm_96' for Fruitfly Ensembl version 96 to build CHiP-seq index files`)
+  seqidx -t chip -a ensembl -r 96 -s dm -d ensembl_dm_96
+    use existing Ensembl annotation files under directory 'ensembl_dm_96' for Fruitfly Ensembl version 96 to build CHiP-seq index files`)
 		fmt.Println()
 	}
 
 	flag.Parse()
 
-	if *filesExisted { // using existing files
-		if _, err := os.Stat(*annotationFileDir); os.IsNotExist(err) {
+	if *existingDir != "" { // using existing files
+		if _, err := os.Stat(*existingDir); os.IsNotExist(err) {
 			log.Fatalf("Please use -d to specify an exsiting directory that contains all annotation files needed")
 		}
-	} else {
-		*annotationFileDir = ""
 	}
 
 	var genomeAnnotations seq.GenomeAnnotations
 	if *genomeSourcePtr == "gencode" {
-		genomeAnnotations = seq.GencodeGenomeAnnotations{Species: *speciesPtr, Version: *rVerPtr, ExistingBaseDir: *annotationFileDir}
+		genomeAnnotations = seq.GencodeGenomeAnnotations{Species: *speciesPtr, Version: *rVerPtr, ExistingBaseDir: *existingDir}
 	} else if *genomeSourcePtr == "ensembl" {
-		genomeAnnotations = seq.EnsemblGenomeAnnotations{Species: *speciesPtr, Version: *rVerPtr, ExistingBaseDir: *annotationFileDir}
+		genomeAnnotations = seq.EnsemblGenomeAnnotations{Species: *speciesPtr, Version: *rVerPtr, ExistingBaseDir: *existingDir}
 	} else {
 		log.Fatalf("Invalid annotation source '%v'\n", *genomeSourcePtr)
 	}
@@ -78,7 +74,7 @@ func main() {
 		log.Fatalf("Invalid sequencing type '%v'\n", *seqType)
 	}
 
-	err := seq.IndexGenomeAnnotations(genomeAnnotations, algorithms, *filesExisted)
+	err := seq.IndexGenomeAnnotations(genomeAnnotations, algorithms, *existingDir != "")
 
 	if err != nil {
 		log.Fatal(err)
